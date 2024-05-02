cde95466e5f863844d63632d9612cffc3abb2cc8
creative.darkstar <creative.darkstar@gmail.com>
2024-05-02 12:17:03 +0900

    Add: added products app's func: pagination

--- spartamarket_DRF/accounts/serializers.py
+++ spartamarket_DRF/accounts/serializers.py
@@ -22,10 +22,15 @@ class UserSerializer(serializers.ModelSerializer):
         return ret
 
     def create(self, validated_data):
+        # validated_data 에서 password 분리
         password = validated_data.pop('password', None)
+        # UserInfo 인스턴스 생성
+        # UserInfo는 AbstractUser를 상속받은 클래스
+        # AbstractUser는 AbstractBaseUser를 상속받은 클래스
         instance = self.Meta.model(**validated_data)
         if password is not None:
-            # provide django, password will be hashing!
+            # AbstractBaseUser 의 method를 사용해 암호화
+            # UserInfo는 AbstractBaseUser 의 method를 사용할 수 있음
             instance.set_password(password)
         instance.save()
         return instance
--- spartamarket_DRF/products/views.py
+++ spartamarket_DRF/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -17,10 +17,31 @@ class ProductListAPIView(APIView):
     # Check user's auth. If it is valid, proceed class funcs.
     permission_classes = [IsAuthenticated]
 
+    # 페이지네이션 상수
+    # 한 페이지에 몇 개의 상품을 표시할지 결정
+    MAX_ITEMS_PER_PAGE = 5
+
     def get(self, request):
-        rows = ProductInfo.objects.filter(is_visible=True)
-        serializer = ProductListSerializer(rows, many=True)
-        return Response(serializer.data)
+        # 쿼리 스트링
+        page = request.query_params.get("page", '1')
+        # 쿼리 스트링이 유효하지 않은 값(숫자가 아닌 경우)일 경우 상태코드 400
+        if not page.isdecimal:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # 페이지네이션 start, end 지정
+        page = int(page)
+        start = (page - 1) * self.MAX_ITEMS_PER_PAGE
+        end = start + self.MAX_ITEMS_PER_PAGE
+
+        # 데이터 페이지네이션
+        rows = ProductInfo.objects.filter(is_visible=True)[start:end]
+        # 유요하지 않은 페이지네이션(데이터가 존재하지 않음)일 경우 상태코드 204
+        # 유효하다면 serialize 진행 후 데이터 응답
+        if not rows:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            serializer = ProductListSerializer(rows, many=True)
+            return Response(serializer.data)
 
     def post(self, request):
         serializer = ProductListSerializer(data=request.data)
@@ -43,6 +64,7 @@ class ProductDetailAPIView(APIView):
 
     def put(self, request, productId):
         row = get_object_or_404(ProductInfo, pk=productId, is_visible=True)
+        # 로그인한 사용자와 상품 등록자가 다를 경우 상태코드 403
         if request.user.id != row.user.id:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
@@ -53,9 +75,12 @@ class ProductDetailAPIView(APIView):
 
     def delete(self, request, productId):
         row = get_object_or_404(ProductInfo, pk=productId, is_visible=True)
+        # 로그인한 사용자와 상품 등록자가 다를 경우 상태코드 403
         if request.user.id != row.user.id:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
+        # sofr delete
+        # 삭제된 상품 정보 추적을 위함
         row.is_visible = False
         row.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
